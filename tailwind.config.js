35a793fc5973b90892420763688f14d6cf27d654
demiroo <demiroezkan205@gmail.com>
2024-11-07 13:20:19 +0100

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   purge: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
+    './components/**/*.{ts,tsx}',
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
